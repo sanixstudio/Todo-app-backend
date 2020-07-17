3a9be6812ec555adfbd65511cf564ec67adab1d6
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-16 17:38:38 -0700

    created Todo Schema Model

---
+++ model/Todo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const TodoSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = mongoose.Model("Todo", TodoSchema);
