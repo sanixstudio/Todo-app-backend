b69dc5553af33a1b2152ee70d43e2e2daa2beabd
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-16 17:33:42 -0700

    created logic to connect to database

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 require("dotenv").config();
 
+// Connection to database
 const mongoose = require("mongoose");
 mongoose
     .connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(() => console.log("Connected to MongoDB"))
+    .then(() => console.log("Database connection successful"))
     .catch((err) => console.log(err));
 
 app.use(cors());
