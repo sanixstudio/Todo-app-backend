afa15cfa701e51988250c40abc1343b78763614b
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-16 21:34:18 -0700

    create route and logic for updating to-do item

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,45 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.post("/", (req, res) => {});
+// save new to-do item to database
+router.post("/add", async (req, res) => {
+    const { text, isCompleted } = req.body;
+    const newTodo = new Todo({ text, isCompleted });
+
+    try {
+        const todo = await newTodo.save();
+        if (!todo)
+            return res
+                .status(400)
+                .json({ msg: "Something went wrong, bad request" });
+        res.status(200).json({ msg: "Saved successfully" });
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+});
+
+// update to-do item
+router.put("/update/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const todoItem = await Todo;
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+});
+
+// delete to-do item
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const removedTodoItem = await Todo.findByIdAndRemove({
+            _id: req.params.id,
+        });
+        if (!removedTodoItem)
+            return res.status(400).json({ msg: "Invalid ID" });
+        res.json({ msg: "Todo item removed" });
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+});
 
 module.exports = router;
